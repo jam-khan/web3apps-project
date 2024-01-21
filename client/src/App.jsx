a8fecbb9a7f209891dfa531cea79ecfe3b1f7be5
jam-khan <jamkhan@connect.hku.hk>
2024-01-20 19:11:11 -0500

    Completed till Navbar. Added some code for other components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+import { Sidebar } from './components';
+// import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+// import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+
+
+const App = () => {
   return (
-    <div className="text-3xl font-bold">
-      Hello
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        {/* <Navbar /> */}
+{/* 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes> */}
+      </div>
     </div>
   )
 }
